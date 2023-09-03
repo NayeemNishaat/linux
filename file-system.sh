adafa18e11020a5dad3307a8848492a07a845403
lAbYriNth <47172193+NayeemNishaat@users.noreply.github.com>
2023-09-03 23:17:22 +0600

    Update file-system.sh

--- file-system.sh
+++ file-system.sh
@@ -1,18 +1,33 @@
+# FILE LIST
 # drwx------@   3 labyrinth  staff    96B Nov 10  2022 Applications # type num of links owner group size month day time name (Directory File)
 # lrwxr-xr-x   1 root  wheel      36 Aug 22 03:37 localtime -> /var/db/timezone/zoneinfo/Asia/Dhaka (Link File)
 # -rw-r--r--@ 1 labyrinth  staff   29615 Jan 19  2023 tbl statement.pdf (Normal File)
 # - -> Regular file, d -> directory file, l -> link file, c -> special/device file, s -> socket file, p -> named pipe file, b -> block device file
-
-# passwd nayeem # Change password for nayeem
-
 # ls -ltr # list all in reverse time order
 
+# CHANGE PASSWORD
+# passwd nayeem # Change password for nayeem
+
+# COPY
 # cp a b # copy a to b (create b if doesn't exist and then copy)
 # vi a # create file name a (in vim :w can be used to save/write current content)
-
 # cp -r test ./abc/test-backup # Note the abc folder must exist
 
+# MOVE
 # mv a ../dir # move file a to directory dir
 
+# DELETE
+# rm file
+# rmdir folder
+
+# SEARCH
 # locate filename # This will give the absolute path of the file
 # find . -name 'file-name' # This will try to find the file from . directory
+# find $(pwd) -name xx # find from cwd
+
+# WILDCARD
+# * -> 0 or more chars (te*)
+# ? -> single chars (te?t)
+# [] -> range of chars (te[a-z]t)
+
+# touch file-{0..9} # create multiple files from 0-9
