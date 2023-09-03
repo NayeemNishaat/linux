db1877b0d1b5ccf819e4b018f2a9899229781a6c
lAbYriNth <47172193+NayeemNishaat@users.noreply.github.com>
2023-09-03 22:17:53 +0600

    Update file-system.sh

--- file-system.sh
+++ file-system.sh
@@ -2,3 +2,7 @@
 # lrwxr-xr-x   1 root  wheel      36 Aug 22 03:37 localtime -> /var/db/timezone/zoneinfo/Asia/Dhaka (Link File)
 # -rw-r--r--@ 1 labyrinth  staff   29615 Jan 19  2023 tbl statement.pdf (Normal File)
 # - -> Regular file, d -> directory file, l -> link file, c -> special/device file, s -> socket file, p -> named pipe file, b -> block device file
+
+# passwd nayeem # Change password for nayeem
+
+# ls -ltr # list all in reverse time order
