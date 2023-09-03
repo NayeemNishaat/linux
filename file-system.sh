adbeee148e75fbf9ffc7149c2b8ae53cfdf8291b
lAbYriNth <47172193+NayeemNishaat@users.noreply.github.com>
2023-09-03 21:46:44 +0600

    Update file-system.sh

--- file-system.sh
+++ file-system.sh
@@ -1,1 +1,4 @@
-# drwx------@   3 labyrinth  staff    96B Nov 10  2022 Applications # type num of links owner group size month day time name
+# drwx------@   3 labyrinth  staff    96B Nov 10  2022 Applications # type num of links owner group size month day time name (Directory File)
+# lrwxr-xr-x   1 root  wheel      36 Aug 22 03:37 localtime -> /var/db/timezone/zoneinfo/Asia/Dhaka (Link File)
+# -rw-r--r--@ 1 labyrinth  staff   29615 Jan 19  2023 tbl statement.pdf (Normal File)
+# - -> Regular file, d -> directory file, l -> link file, c -> special/device file, s -> socket file, p -> named pipe file, b -> block device file
