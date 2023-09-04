ef1d65ef183843c62c1dfcec0967d9d24f251008
lAbYriNth <47172193+NayeemNishaat@users.noreply.github.com>
2023-09-04 16:07:11 +0600

    Update file-permission.sh

--- file-permission.sh
+++ file-permission.sh
@@ -1,3 +1,18 @@
+# ALPHABETIC PERMISSION
+# a -> all, g -> group, o > other, u -> user
+# + -> add, - -> remove
+# r -> read, w -> write, x -> execute
+# Note: drwx the x permission in a directory means it can be visited 
 # chmod a+x file # give execute permission to all
 # chmod g-w file # remove write permission form group
-# Note: drwx the x permission in a directory means it can be visited 
+
+# NUMERIC PERMISSION
+# 0 -> no permission
+# 1 -> execute
+# 2 -> write
+# 3 -> execute + write
+# 4 -> read
+# 5 -> read + execute
+# 6 -> read + write
+# 7 -> read + write + execute
+# chmod 764 file
