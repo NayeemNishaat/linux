968ca34547ed50090f02de919c44df03d699a999
lAbYriNth <47172193+NayeemNishaat@users.noreply.github.com>
2023-09-04 16:30:03 +0600

    Update file-permission.sh

--- file-permission.sh
+++ file-permission.sh
@@ -16,3 +16,11 @@
 # 6 -> read + write
 # 7 -> read + write + execute
 # chmod 764 file
+# chmod 600 file
+
+# CHANGE OWNER
+# Important: Only root user can change ownership of a file
+# chown root Admanager.pdf
+# chgrp staff Admanager.pdf
+
+# Warning: Although a file is owned by root and no permission to user/group/other this file can still be deleted if the parent directories are owned by the current user and has appropriate permissions.
