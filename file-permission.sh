337a498ac4d6ded41ab5d0ea4d7778241d12145f
lAbYriNth <47172193+NayeemNishaat@users.noreply.github.com>
2023-09-05 16:54:35 +0600

    Update file-permission.sh

--- file-permission.sh
+++ file-permission.sh
@@ -44,3 +44,14 @@
 # echo 'abc def' >> file # append to file
 # ls -l > file # redirect output to file (overwrite)
 # ls -l >> file # redirect output to file (append)
+
+# REDIRECTS
+# Remark: everything in linux is a file
+# stdin -> 0 (file descriptor)
+# stdout -> 1
+# stderr -> 2
+# cat < file # feed cat from file
+# mail -s 'memo' abc@mail.com < letter (< is input sign)
+# cat root  2>> file # redirect std err to file (Warning: there should be no space between 2 and >>)
+# cat root  2> file
+# ls >> file 2>&1 # redirect std out and std err to file
