2fbc1d19784d0e155429ae8499614d39d9a3ea0a
lAbYriNth <47172193+NayeemNishaat@users.noreply.github.com>
2023-09-05 16:27:02 +0600

    Update file-permission.sh

--- file-permission.sh
+++ file-permission.sh
@@ -33,3 +33,14 @@
 # setfacl -b /path/to/file # remove all entries for all users
 # write permission with acl won't allow to delete the file but write. + will be added if contains acl in permission column
 # getfacl file # get file acl
+
+# HELP COMMANDS
+# whatis ls # short description about the command
+# ls --help # show help manu of the cmd
+# man ls # show the manual of the cmd
+
+# WRITE TO FILE
+# echo 'abc def' > file # overwrite to file
+# echo 'abc def' >> file # append to file
+# ls -l > file # redirect output to file (overwrite)
+# ls -l >> file # redirect output to file (append)
