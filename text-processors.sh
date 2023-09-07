38e072a573510776bb9b0436a9e833bddcb1d04f
lAbYriNth <47172193+NayeemNishaat@users.noreply.github.com>
2023-09-07 13:36:00 +0600

    Update text-processors.sh

--- text-processors.sh
+++ text-processors.sh
@@ -30,8 +30,15 @@
 # GREP
 # grep abc file # find abc in file
 # grep -c it file # will just return the count
-# grep -i fuck file # ignore case
+# grep -i abc file # ignore case
 # grep -in it file # return result with line number
 # grep -v it file get all the lines where 'it' doesn't exist
 # grep -ivn it file | awk '{print $1}' # get first col and all rows of grep
 # grep -ivn it file | awk 'FNR==1 {print}' # get first row and all cols of grep
+# grep -ivn it file | awk 'FNR==1 {print $2}' # 1st row 2nd col
+# grep -ivn it file | awk 'FNR==1 {print $0}' | cut -c 3- # grep it awk it and cut it from 3 to end
+# ll | grep file$ # return lines where the lines contain words that end with file
+
+# EGREP
+# Note: Used for searching multiple words
+# egrep -i "it|abc" file # will search it and abc
