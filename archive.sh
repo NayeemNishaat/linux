69c911383251d9ca10d5d2b6c4fc192fba08f587
lAbYriNth <47172193+NayeemNishaat@users.noreply.github.com>
2023-09-07 17:00:10 +0600

    Create archive.sh

---
+++ archive.sh
@@ -0,0 +1,13 @@
+# TAR
+# tar -cvf f.tar . # create tar archive with compress and verbose
+# tar --exclude tar  -cvf f.tar . # exclude a file
+# tar -tvf f.tar # show the contents of tar file
+# tar -xvf f.tar # extract from tar ball
+# tar -czvf f.gz .
+
+# GZIP
+# gzip f.tar # compress tar file
+# gzip -9 f.tar # highest compression level
+# gzip -d f.tar.gz # decompress tar file
+# gunzip f.tar.gz # decompress tar file
+# tar -czvf d.gz . # gzip with compression level 1
