07b8565f3ab9345beb39a51f33b62ef3f1145b42
lAbYriNth <47172193+NayeemNishaat@users.noreply.github.com>
2023-09-09 22:38:43 +0600

    Update basic-cmds.sh

--- vim/basic-cmds.sh
+++ vim/basic-cmds.sh
@@ -3,3 +3,10 @@
 # u # undo
 # :w # write/save
 # shift + z z # write and quit
+# yy # yank/copy line
+# Y # copy line
+# yw # copy word
+# p # paste/put after cursor
+# P # put before cursor
+# v # visual mode
+# go to visual mode use arrows to highlight and press y to yank highlighted text and use p to put.
