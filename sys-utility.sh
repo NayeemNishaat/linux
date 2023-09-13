81c6a1cac1fae711d86a3f391c8ea72f009f79ba
lAbYriNth <47172193+NayeemNishaat@users.noreply.github.com>
2023-09-13 15:55:44 +0600

    Create sys-utility.sh

---
+++ sys-utility.sh
@@ -0,0 +1,23 @@
+# DATE
+# date # get date and time
+
+# UPTIME
+# uptime # will provide system uptime info current time, how long system has been running, active users, avg jobs in system run queue for 1, 5, 15 minutes
+
+# HOSTNAME
+# hostname # return host/device name
+
+# UNAME
+# uname # return machine type (distro)
+# uname -a # return machine type (distro)
+
+# WHICH
+# which sh # get location of the command
+
+# CAL
+# cat # return current month's calendar
+# cal 12 1997 # return december 1997 month's calendar
+# cal 1997 # get calendar of 1997
+
+# BC
+# bc # opens calculator and provide expression to evaluate
