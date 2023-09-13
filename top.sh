b5da32e1858277f2de82f4dd1c6cd81b8248d6d2
lAbYriNth <47172193+NayeemNishaat@users.noreply.github.com>
2023-09-13 17:02:33 +0600

    Create top.sh

---
+++ top.sh
@@ -0,0 +1,20 @@
+# top # shows realtime process info
+# pid -> process id
+# user -> username
+# pr -> priority
+# ni -> nice value; negative value means higher priority; positive less
+# virt -> virtual memory used by the task
+# res -> ram consumed by the process
+# shr -> represents the shared memory used by the task
+# s -> shows the process state in single letter form
+# %cpu -> cpu usage % by the process
+# %mem -> ram usage % by the process
+# time -> cpu time
+# command -> command running the process
+
+# top -u nayeem
+# top (press 'c' in interactive mode) # show absolute path of the command
+# top (press 'k' in interactive mode and enter pid) # kill the process with the pid
+# top (press 'M' and then 'P' in interactive mode) # sort processes by memory usage
+
+# Note: top refreshes the screen in every 3 sec
