35240c57c764fd1b9da37428225c49cb971aa466
lAbYriNth <47172193+NayeemNishaat@users.noreply.github.com>
2023-09-13 17:15:24 +0600

    Create kill.sh

---
+++ kill.sh
@@ -0,0 +1,8 @@
+# kill -l # list all signal names
+# kill pid # kill process with pid that will send default signal
+# kill -1 pid # restart process
+# kill -2 pid # send interrupt signal (^C)
+# kill -9 pid # force kill process
+# kill -15 pid # kill gracefully
+# killall # delete all processes and child processes
+# pkill # kill by process name
