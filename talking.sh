28807c8174cd5f15ccc5b740755dbf7bc39ff712
lAbYriNth <47172193+NayeemNishaat@users.noreply.github.com>
2023-09-13 14:16:47 +0600

    Update talking.sh

--- talking.sh
+++ talking.sh
@@ -1,2 +1,8 @@
 # WALL
 # wall # after running the command write the message and go to a new line then to send the message hit control+d
+
+# USERS
+# users # get active users
+
+# WRITE
+# write laby # will send message to laby only (control+d to stop sending)
