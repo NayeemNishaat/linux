3ff2558e251537a7e21ee30bc548dfabbc13f718
lAbYriNth <47172193+NayeemNishaat@users.noreply.github.com>
2023-09-13 17:41:57 +0600

    Create at.sh

---
+++ at.sh
@@ -0,0 +1,9 @@
+# at 05:02PM (hit enter will go to interactive mode and provide the command there after that go to a new line and then press control+d)
+# atq # show entries in at queue
+# atrm id # remove at entry with the id from atq
+# systemctl status atd
+# at 2:45 AM 101621 # schedule on 16th Oct 2021 at 2:45am
+# at 4PM + 4 days # schedule a job at 4pm four days from now
+# at now +5 hours # run 5 hours later from now
+# at 8:00 AM Sun # run at 8am on coming sunday
+# at 10:00 AM next month # run at 8am next month
