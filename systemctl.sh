2073a83f89c7f9237ec859efd75d2d5a4def204a
lAbYriNth <47172193+NayeemNishaat@users.noreply.github.com>
2023-09-13 16:20:33 +0600

    Create systemctl.sh

---
+++ systemctl.sh
@@ -0,0 +1,12 @@
+# systemctl status firewalld # get status info of firewalld.service
+# systemctl stop firewalld.service # stop firewalld.service
+# systemctl enable firewalld # start firewalld.service at boot
+# systemctl disable firewalld # don't start firewalld.service at boot
+# systemctl restart nginx # restart nginx
+# systemctl reload nginx # reload nginx
+# systemctl list-units # list all active services
+# systemctl list-units --all # list all services
+
+# systemctl poweroff
+# systemctl halt
+# systemctl reboot
