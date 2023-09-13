327a8eca97c3e84316e426c736e8d99c59322d54
lAbYriNth <47172193+NayeemNishaat@users.noreply.github.com>
2023-09-13 17:26:52 +0600

    Create crontab.sh

---
+++ crontab.sh
@@ -0,0 +1,6 @@
+# crontab -l # list crontabs
+# crontab -e # edit crontab
+# crontab -r # remove everything from crontab
+# systemctl status crond # show status of the cron daemon
+
+# 21 16 * 10 * echo "Cron Test" > cron.log # default location is home dir
