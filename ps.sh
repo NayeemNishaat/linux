a5ae60a0ea00221df0005a83dd0b18b9848ad065
lAbYriNth <47172193+NayeemNishaat@users.noreply.github.com>
2023-09-13 16:34:49 +0600

    Create ps.sh

---
+++ ps.sh
@@ -0,0 +1,7 @@
+# ps # shows the processes of the current shell showing process id, tty (terminal type), cpu time, cmd
+# ps -e # shows all running processes
+# ps aux # shows all running processes in BSD format
+# ps ax # shows all running processes
+# ps -ef # shows all running processes in full format listing
+# ps -u root # get all processes of root
+# ps -ef | grep 123
