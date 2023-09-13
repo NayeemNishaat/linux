aa534d77b267c0f9892fa4a673fa53d655734305
lAbYriNth <47172193+NayeemNishaat@users.noreply.github.com>
2023-09-13 13:49:06 +0600

    Update monitor-user.sh

--- monitor-user.sh
+++ monitor-user.sh
@@ -1,8 +1,18 @@
 # WHO
 # who # this will tell how many users are logged in and their username, terminal id and date (pts/tty count provides the # of opened terminals)
+
+# LAST
 # last # this will provide the login history
+
+# W
 # w # same as who but with more info
+
+# FINGER
 # finger # provide info of logged in users
+
+# ID
 # id # provides info of the current user
 # id root # provides info of the root user
+
+# USERS
 # users # get active users
