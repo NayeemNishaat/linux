086d146e4ae7152458131a68062814715aabafd2
lAbYriNth <47172193+NayeemNishaat@users.noreply.github.com>
2023-09-14 16:03:22 +0600

    Create sys-maintenance.sh

---
+++ sys-maintenance.sh
@@ -0,0 +1,17 @@
+# SHUTDOWN
+# shutdown -H # force shutdown immediately
+# shutdown -P # poweroff
+# shutdown -r # reboot
+
+# INIT
+# init 6 # reboot (init has options from 0-6)
+
+# HALT
+# halt # force shutdown immediately
+
+# REBOOT
+# reboot # reboot device
+
+# HOSTNAMECTL
+# hostnamectl - set-hostname newhostname
+# Note: hostname is stored in /etc/hostname (linux 7)
