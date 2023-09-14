0da73f69ffd0618514d1cf6757cf1361147472e8
lAbYriNth <47172193+NayeemNishaat@users.noreply.github.com>
2023-09-14 15:45:32 +0600

    Create log-monitoring.sh

---
+++ log-monitoring.sh
@@ -0,0 +1,12 @@
+# log dir -> /var/log
+# more /var/log/system.log
+# more /var/log/boot.log
+# ll /var/log/chrony
+# cat /var/log/cron
+# cat /var/log/dmesg
+# cat /var/log/maillog
+# more /var/log/secure
+# tail -f /var/log/secure # keep monitoring the log file
+# more /var/log/messages # get whole device logs
+# grep -i error /var/log/messages
+# journalctl -u cron.service
