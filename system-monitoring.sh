519bf53821d512ad6cb7a0b3c74ad506bb100ca0
lAbYriNth <47172193+NayeemNishaat@users.noreply.github.com>
2023-09-14 15:12:12 +0600

    Create system-monitoring.sh

---
+++ system-monitoring.sh
@@ -0,0 +1,10 @@
+# top
+# df # show disk info
+# du -h -d 1 # show disk usage by folder
+# dmesg # show kernel logs
+# iostat # get input output stats
+# iostat 1 # get input output stats refresh per sec.
+# netstat -rnv # get network info
+# free -h # will provide physical and virtual memory status
+# cat /proc/cpuinfo # get cpu info
+# cat /proc/meminfo # get memory info
