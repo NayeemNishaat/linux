3923566dfc11c8eadb2fa6656984368fa4b94ca6
lAbYriNth <47172193+NayeemNishaat@users.noreply.github.com>
2023-09-15 16:08:32 +0600

    Update terminal-utility.sh

--- terminal-utility.sh
+++ terminal-utility.sh
@@ -3,3 +3,17 @@
 # control + c # stop/kill command
 # control + z # suspend command
 # control + d # exit command
+
+# CLEAR
+# clear # clean screen
+
+# EXIT
+# exit # exit out of shell
+
+# SCRIPT
+# script script_name.log # will create a script file containing all the logs of the user session
+
+# HISTORY
+# history # will provide user session history
+# history -23 # will remove history number 23
+# history -c # will clear entire history
