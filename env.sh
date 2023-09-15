92808bb6b0e855f57c19de40efe05371201163fb
lAbYriNth <47172193+NayeemNishaat@users.noreply.github.com>
2023-09-15 18:24:40 +0600

    Create env.sh

---
+++ env.sh
@@ -0,0 +1,19 @@
+# ENV
+# env
+
+# PRINTENV
+# printenv
+
+# EXPORT
+# export TEST=1 # set env in current session
+# echo $TEST # print env value
+
+# SET ENV PERMANENTLY
+# vi .bashrc
+# TEST='123'
+# export TEST
+
+# SET ENV GLOBLLY AND PERMANENTLY
+# vi /etc/profile or vi /etc/bashrc
+# TEST='123'
+# export TEST
