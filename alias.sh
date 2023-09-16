a4c0108fffa0989b30b3f248d1102918d55c5b7f
lAbYriNth <47172193+NayeemNishaat@users.noreply.github.com>
2023-09-16 19:44:49 +0600

    Create alias.sh

---
+++ alias.sh
@@ -0,0 +1,11 @@
+# SESSION
+# alias ls="ls -al"
+# alias d="df -h | awk '{print \$9}' | cut -c1-4"
+# alias ps="pwd;ls;"
+
+# UNALIAS
+# unalias d
+
+# PERMANENT
+# User -> /home/username/.bashrc (add user level alias here)
+# Global -> /etc/bashrc (add global alias here)
