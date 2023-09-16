c5619aa118ef132af642664684fb0db7f42a09e6
lAbYriNth <47172193+NayeemNishaat@users.noreply.github.com>
2023-09-17 01:11:30 +0600

    Create networking.sh

---
+++ networking.sh
@@ -0,0 +1,16 @@
+# cat /etc/nsswitch.conf
+# cat /etc/hosts
+# cat /etc/sysconfig/network
+# cd /etc/sysconfig/network-scripts
+# vi ifcfg-enp0s3
+# cat /etc/resolv.conf
+
+# vi /etc/hosts (add new entry 192.168.0.1 mydomain)
+# ping mydomain
+# ifconfig
+# ifup
+# ifdown
+# netstat -rnv
+# tcpdump -i enp0s3
+
+# ethtool enp0s3
