0c4249796c4e76fd8117984684ce9a84469b6b9a
lAbYriNth <47172193+NayeemNishaat@users.noreply.github.com>
2023-09-19 12:16:31 +0600

    Create curl.sh

---
+++ curl.sh
@@ -0,0 +1,14 @@
+# CURL
+# curl google.com # get google.com
+# curl -L google.com # follow redirection
+# curl -LO https://file.com/download # download file use remote name (-O)
+# curl "http://{site,host}.host[1-5].com" -o "#1_#2" # save file to #1_#2
+# curl -fsSL https://bun.sh/install | bash # -f -> fail early, s-> silent, S -> show error, L -> follow
+
+# PING
+# Note: ping will only check if server is up not the website
+# ping 172.217.170.206
+# ping google.com
+
+# NSLOOKUP
+# nslookup 172.217.170.206 # get ip info
