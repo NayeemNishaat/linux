f86b165648736e2033d63f103141892462aae33d
lAbYriNth <47172193+NayeemNishaat@users.noreply.github.com>
2023-09-19 12:58:32 +0600

    Create scp.sh

---
+++ scp.sh
@@ -0,0 +1,5 @@
+# Note: check if sshd is running
+# Note: check if port 22 is open
+# ifconfig interface # get the ip of the server interface
+# scp file remote@193.23.56.21:/home/user
+# Note: enter the server password to transfer
