f4b36fee74a1c5710b22880313f08d3d848b900b
lAbYriNth <47172193+NayeemNishaat@users.noreply.github.com>
2023-09-19 13:00:00 +0600

    Update scp.sh

--- scp.sh
+++ scp.sh
@@ -1,5 +1,5 @@
 # Note: check if sshd is running
 # Note: check if port 22 is open
 # ifconfig interface # get the ip of the server interface
-# scp file remote@193.23.56.21:/home/user
+# scp file remote@193.23.56.21:/home/user # username@server_ip:location_in_server
 # Note: enter the server password to transfer
