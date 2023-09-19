c4c0412efe71c11ca618c1f3a3659fec3273b016
lAbYriNth <47172193+NayeemNishaat@users.noreply.github.com>
2023-09-19 16:06:34 +0600

    Create pkg-manager.sh

---
+++ pkg-manager.sh
@@ -0,0 +1,6 @@
+# yum install httpd # redhat
+# apt-get install # other distros
+# rpm -qa # list all installed packages
+# rpm -hiv location.rpm # install downloaded rpm pkg
+# rpm -e pkg
+# yum remove bind # uninstall a pkg
