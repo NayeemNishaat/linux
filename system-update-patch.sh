d1eddf6a6db1ed2f16e20aaa38942b1489840e63
lAbYriNth <47172193+NayeemNishaat@users.noreply.github.com>
2023-09-20 10:06:52 +0600

    Create system-update-patch.sh

---
+++ system-update-patch.sh
@@ -0,0 +1,4 @@
+# yum update # preserve old package and add new
+# yum update -y
+
+# yum upgrade # deletes old package and installs new version where update just installs new version doesn't delele old package
