88d27effd4b94f1efc39e4fb72115a854a2f8612
lAbYriNth <47172193+NayeemNishaat@users.noreply.github.com>
2023-09-20 10:34:12 +0600

    Update pkg-manager.sh

--- pkg-manager.sh
+++ pkg-manager.sh
@@ -4,3 +4,19 @@
 # rpm -hiv location.rpm # install downloaded rpm pkg
 # rpm -e pkg
 # yum remove bind # uninstall a pkg
+
+# SET LOCAL REPO ON RED HAT
+# cd /etc/yum.repos.d
+# rm -rf *
+# vi local.repo # create repo file in /etc/yum.repos.d
+[centos7]
+name=centos7
+baseurl=file:///localrepo # /localrepo is the repo location
+enabled=1
+gpgcheck=0
+
+# install createrepo
+# createrepo /localrepo
+# yum clean all # clear cache from old repo
+# yum repolist all # get all repo list
+# yum install tomcat # test local repo
