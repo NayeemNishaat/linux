0bf540da2ca94a14f099e61fbf25f4dae0f07a8a
lAbYriNth <47172193+NayeemNishaat@users.noreply.github.com>
2023-09-20 10:20:54 +0600

    Update sys-info.sh

--- sys-info.sh
+++ sys-info.sh
@@ -6,3 +6,19 @@
 
 # ARCH
 # arch # returns system architecture
+
+# DU
+# du -sh . # get current dir size
+# du -h .
+# df # show disk info
+# du -h -d 1 # show disk usage by folder level 1
+
+# CPU INFO
+# cat /proc/cpuinfo # get cpu info
+
+# MEMORY INFO
+# cat /proc/meminfo # get memory info
+# free -h # will provide physical and virtual memory status
+
+# NETWORK INFO
+# netstat -rnv # get network info
