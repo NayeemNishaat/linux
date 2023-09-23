1ee8843d5d4b3b8dc8665d71285d04700f49ab23
lAbYriNth <47172193+NayeemNishaat@users.noreply.github.com>
2023-09-23 13:34:27 +0600

    Create ntp.sh

---
+++ ntp.sh
@@ -0,0 +1,6 @@
+# yum install ntp
+# cat /etc/ntp.conf
+# systemctl start ntpd
+# systemctl restart ntpd
+# Note: ntp port is 123
+# ntpq # type peers in prompt and this will show list of time servers
