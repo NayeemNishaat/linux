b3f51312e0a9bcd4824d767c6fcc6fd4f5727c6b
lAbYriNth <47172193+NayeemNishaat@users.noreply.github.com>
2023-09-23 15:57:50 +0600

    Create http.sh

---
+++ http.sh
@@ -0,0 +1,10 @@
+# APACHI
+# yum install httpd
+# cat /etc/httpd/conf/httpd.conf
+# /var/www/html/index.html
+# disable firewall
+# visit the ip of the active internet interface
+# systemctl start httpd
+
+# NGINX
+# yum install nginx
