c1d9bc47e14e2448ce3560d30b4902eeec64ef56
lAbYriNth <47172193+NayeemNishaat@users.noreply.github.com>
2023-09-23 15:06:26 +0600

    Create mail.sh

---
+++ mail.sh
@@ -0,0 +1,7 @@
+# yum install sendmail
+# yum install sendmail-cf
+# cd /etc/mail # mail config dir
+
+# vi sendmail.mc # provide smtp server info in SMART_HOST
+
+# mail -s 'subject' email@mail.com # provide email body and go to new line and press control + d to send.
