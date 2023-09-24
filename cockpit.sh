ec19c2abab359cca0e5fcf2350154734e719541d
lAbYriNth <47172193+NayeemNishaat@users.noreply.github.com>
2023-09-24 20:24:23 +0600

    Create cockpit.sh

---
+++ cockpit.sh
@@ -0,0 +1,6 @@
+# yum/dnf install cockpit # install cockpit
+# apt-get install cockpit
+
+# systemctl start/enable cockpit
+
+# server_ip:9090 # access the web interface on port 9090 of the server ip
