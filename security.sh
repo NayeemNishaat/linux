4171695eb0fb97f1dfd84ed4efd4edf643c396c3
lAbYriNth <47172193+NayeemNishaat@users.noreply.github.com>
2023-09-24 10:40:12 +0600

    Update security.sh

--- security.sh
+++ security.sh
@@ -1,4 +1,4 @@
-# Choose an uncommon username
+# Note: Choose an uncommon username
 # chage -l username # update password change policy
 # cat /etc/shadow # password info
 # cat /etc/login.defs # default login stats
@@ -7,3 +7,14 @@
 # netstat -tunlp # list all open listening ports
 # netstat -tunlp tcp # list all open listening ports (tcp)
 # lsof -i :63937 # find what is running on a port
+# cat /etc/ssh/sshd_config # view ssh config (permit root login -> no)
+# Note: enable firewall
+# ufw allow 22
+
+# SELINUX
+# sestatus
+# cat /etc/sysconfig/selinux
+# man chcon
+# man checkpolicy
+# stat filename # get file stats
+
