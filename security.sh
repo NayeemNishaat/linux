0ccf114f0409710558a0c4c9f752979667362e98
lAbYriNth <47172193+NayeemNishaat@users.noreply.github.com>
2023-09-24 09:59:30 +0600

    Create security.sh

---
+++ security.sh
@@ -0,0 +1,9 @@
+# Choose an uncommon username
+# chage -l username # update password change policy
+# cat /etc/shadow # password info
+# cat /etc/login.defs # default login stats
+# cat /etc/pam.d/system-auth
+# systemctl -a # list all services
+# netstat -tunlp # list all open listening ports
+# netstat -tunlp tcp # list all open listening ports (tcp)
+# lsof -i :63937 # find what is running on a port
