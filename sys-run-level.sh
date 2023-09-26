769521e08d71a6f6e16a038f7f331c03fe3ba2b4
lAbYriNth <47172193+NayeemNishaat@users.noreply.github.com>
2023-09-26 16:48:14 +0600

    Create sys-run-level.sh

---
+++ sys-run-level.sh
@@ -0,0 +1,9 @@
+init o -> shut down/halt
+init 1 -> single user mode
+init -s -> single user mode
+init 6 -> reboot
+init 2 -> multiuser mode without networking
+init 3 -> multiuser mode with networking but no gui
+init 5 -> multiuser mode with networking and gui
+
+# who -r # get run level
