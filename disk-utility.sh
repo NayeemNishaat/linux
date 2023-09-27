8ac730dcd2edc76dde90cdc55ca5bdfd7d093087
lAbYriNth <47172193+NayeemNishaat@users.noreply.github.com>
2023-09-27 16:17:19 +0600

    Update disk-utility.sh

--- disk-utility.sh
+++ disk-utility.sh
@@ -8,3 +8,6 @@
 # du -h -d 1 # get current folder’s sub folders’s  size
 # du -ha # get all sub file’s size
 # du -hd 1 -t 300M # get files greater than 500M at depth 1 (all dir/file of current dir)
+
+# FDISK
+# fdisk -l # show disk partitions
