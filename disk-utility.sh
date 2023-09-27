92734d2534eeeac56553ccee013c36d034d35bf8
lAbYriNth <47172193+NayeemNishaat@users.noreply.github.com>
2023-09-27 16:40:45 +0600

    Update disk-utility.sh

--- disk-utility.sh
+++ disk-utility.sh
@@ -6,8 +6,34 @@
 # du -ha # get all sub file’s size
 # du -hd 1 -t 300M # get files greater than 500M at depth 1 (all dir/file of current dir)
 
+# DF
+# df -h # get disk info in human readable format
+
 # FDISK
 # fdisk -l # show disk partitions
 
-# DF
-# df -h # get disk info in human readable format
+# Note: Create a new partition
+# fdisk /dev/sdb # this will go to interactive mode (type help/m to get help menu)
+# In the interactive shell:
+# n -> add new partition
+# p -> primary partition type
+# Rest are default
+# w -> write table to disk and exit
+
+# Note: Create a new File System from the partition
+# fdisk -l # show available volums
+# mkfs.xfs /dev/sdb1 # make file system from a volume/partition
+
+# Note: Mount the file system
+# mkdir /mount_point
+# mount /dev/sdb1 /mount_point
+# df -h
+
+# Note: Make the mount persistent
+# vi /etc/fstab # edit this file as follows,
+# /dev/sdb1  /data  xfs  defaults  0  0
+# init 6 # reboot
+
+# Note Unmount
+# umount /data
+# mount -a # will mount all described in /etc/fstab
