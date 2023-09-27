8162062acfc162d7cd1d0cf00ac181193fcdff4d
lAbYriNth <47172193+NayeemNishaat@users.noreply.github.com>
2023-09-27 14:02:32 +0600

    Create disk-utility.sh

---
+++ disk-utility.sh
@@ -0,0 +1,10 @@
+# DF
+# df -h # get disk info in human readable format
+
+# DU
+# du -sh # get current folder size
+# du -sh /Users/labyrinth/Downloads # get /Users/labyrinth/Downloads folder size
+# du -hd 0 # get current folder size only
+# du -h -d 1 # get current folder’s sub folders’s  size
+# du -ha # get all sub file’s size
+# du -hd 1 -t 300M # get files greater than 500M at depth 1 (all dir/file of current dir)
