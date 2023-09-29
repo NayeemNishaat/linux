128a5ac61b4ceab81e53a9f98da44d1779e344f2
lAbYriNth <47172193+NayeemNishaat@users.noreply.github.com>
2023-09-29 15:21:04 +0600

    Create nfs.sh

---
+++ nfs.sh
@@ -0,0 +1,17 @@
+# yum install nfs-utils libnfsidmap
+# systemctl enable rpcbind
+# systemctl enable nfs-server
+# systemctl start rpcbind
+# systemctl start nfs-server
+# systemctl start rpc-statd
+# systemctl start nfs-idmapd
+
+# mkdir /nfs-dir
+# chmod a+rwx /nfs-dir
+
+# ADD NFS SHARED FS
+# vi /etc/exports # add following line:
+# /nfs-dir 192.168.12.3 (rw,sync,no_root_squash) # for a specific ip
+# /nfs-dir * (rw,sync,no_root_squash) # for all ips
+
+# exportfs -rv # export the nfs fs
