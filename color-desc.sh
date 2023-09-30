7f4936cac906777548e53a7971aa36ae08883234
lAbYriNth <47172193+NayeemNishaat@users.noreply.github.com>
2023-09-30 16:25:03 +0000

    Create color-desc.sh

---
+++ color-desc.sh
@@ -0,0 +1,7 @@
+# blue -> Directory
+# green -> Executable/recognized data file
+# Sky Blue -> Symbolic Link File
+# Yellow with black background -> Device
+# Pink -> Image File
+# Red -> Archive File (tar)
+# Red with black background -> Broken Link
