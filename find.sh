245b6119a26b9889b763de17c5a09d4c561bbc2c
lAbYriNth <47172193+NayeemNishaat@users.noreply.github.com>
2023-10-02 05:58:19 +0000

    Update find.sh

--- find.sh
+++ find.sh
@@ -1,1 +1,6 @@
 # find . -type f -size +50M -exec du -h {} \; | sort -n # find files greater than 50M and execute du -h for each line and finally sort by numerically
+# find . -name 'pattern' # basic find
+# find . ! -name 'pattern'
+# find . -name "*.vtt" ! -name "*English*" -delete # find all .vtt except one containing english and delete
+# find ../.. -samefile fxx # find hardlinks
+# find ../.. -inum 20948039 # find file with same inode number (hard link)
