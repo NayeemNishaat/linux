0e53ffd5b13a8f9251d0819f051fb7aced527527
lAbYriNth <47172193+NayeemNishaat@users.noreply.github.com>
2023-10-02 06:05:06 +0000

    Update find.sh

--- find.sh
+++ find.sh
@@ -4,3 +4,4 @@
 # find . -name "*.vtt" ! -name "*English*" -delete # find all .vtt except one containing english and delete
 # find ../.. -samefile fxx # find hardlinks
 # find ../.. -inum 20948039 # find file with same inode number (hard link)
+# find . -name *.js -size +1M -ls
