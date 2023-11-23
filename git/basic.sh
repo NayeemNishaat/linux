f7ed9c3480aff435be2c38c981fe4cbd7cde9b4c
lAbYriNth <47172193+NayeemNishaat@users.noreply.github.com>
2023-11-23 23:41:40 +0600

    Create basic.sh

---
+++ git/basic.sh
@@ -0,0 +1,5 @@
+# STASH
+git stash drop            # drop top hash, stash@{0}
+git stash drop stash@{n}  # drop specific stash
+git stash list            # Show stash list
+git stash clear           # Drop all stashes
