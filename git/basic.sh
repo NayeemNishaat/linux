73e0d8cdce2a6bb24bbdd10140fd48304fafac24
lAbYriNth <47172193+NayeemNishaat@users.noreply.github.com>
2023-11-26 13:13:01 +0600

    Update basic.sh

--- git/basic.sh
+++ git/basic.sh
@@ -8,4 +8,13 @@ git stash clear           # Drop all stashes
 git gc --aggressive --prune=now
 
 # REBASE
-# git rebase --root
+git rebase --root
+
+# Remove All Old Commits
+git checkout --orphan newBranch
+git add -A  # Add all files and commit them
+git commit
+git branch -D master  # Deletes the master branch
+git branch -m master  # Rename the current branch to master
+git push -f origin master  # Force push master branch to github
+git gc --aggressive --prune=all     # remove the old files
