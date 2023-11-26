3e90d91eef4db64aa5e0892a8f90a30c79a5ef8f
lAbYriNth <47172193+NayeemNishaat@users.noreply.github.com>
2023-11-26 13:05:36 +0600

    Update basic.sh

--- git/basic.sh
+++ git/basic.sh
@@ -3,3 +3,6 @@ git stash drop            # drop top hash, stash@{0}
 git stash drop stash@{n}  # drop specific stash
 git stash list            # Show stash list
 git stash clear           # Drop all stashes
+
+# Cleanup
+git gc --aggressive --prune=now
