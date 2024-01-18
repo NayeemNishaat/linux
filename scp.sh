d82ed1ed3538de0be7bd3c860edd79fff8a56a1d
lAbYriNth <47172193+NayeemNishaat@users.noreply.github.com>
2024-01-19 02:30:28 +0600

    Update scp.sh

--- scp.sh
+++ scp.sh
@@ -3,3 +3,4 @@
 # ifconfig interface # get the ip of the server interface
 # scp file remote@193.23.56.21:/home/user # username@server_ip:location_in_server
 # Note: enter the server password to transfer
+scp root@1.2.3.4:/root/pcfilename.rar ./
