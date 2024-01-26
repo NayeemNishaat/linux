db8ec8ec0ecb14f8149b34ef44be032743cdbc61
lAbYriNth <47172193+NayeemNishaat@users.noreply.github.com>
2024-01-26 23:34:37 +0600

    Update submodule.sh

--- git/submodule.sh
+++ git/submodule.sh
@@ -2,7 +2,6 @@ Clone the sub repo to the desired folder.
 Add that folder to the parent git repo, git submodule add http://23.29.118.76:3000/mkdlabs/auction_frontend.git /path/to/folder
 Or directly add the submodule to git, git submodule add http://23.29.118.76:3000/mkdlabs/auction_frontend.git
 Or directly add the submodule to git, git submodule add http://23.29.118.76:3000/mkdlabs/auction_frontend.git .
-Or directly add the submodule to git, git submodule add http://23.29.118.76:3000/mkdlabs/auction_frontend.git /path/to/folder
 
 git submodule update
 git submodule init
