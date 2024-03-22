c307fda3f3654c028134276cab95e00c1c4ea1f6
lAbYriNth <47172193+NayeemNishaat@users.noreply.github.com>
2024-03-23 04:47:11 +0600

    Update find.sh

--- find.sh
+++ find.sh
@@ -1,8 +1,10 @@
-# find . -type f -size +50M -exec du -h {} \; | sort -n # find files greater than 50M and execute du -h for each line and finally sort by numerically
-# find . -name 'pattern' # basic find
-# find . ! -name 'pattern'
-# find . -name "*.vtt" ! -name "*English*" -delete # find all .vtt except one containing english and delete
-# find ../.. -samefile fxx # find hardlinks
-# find ../.. -inum 20948039 # find file with same inode number (hard link)
-# find . -name *.js -size +1M -ls
-# sudo find . -name '*WenttoPakista*.mp4' -o -name '*IAMtheeart*.mp4'
+find . -type f -size +50M -exec du -h {} \; | sort -n # find files greater than 50M and execute du -h for each line and finally sort by numerically
+find . -name 'pattern' # basic find
+find . ! -name 'pattern'
+find . -name "*.vtt" ! -name "*English*" -delete # find all .vtt except one containing english and delete
+find ../.. -samefile fxx # find hardlinks
+find ../.. -inum 20948039 # find file with same inode number (hard link)
+find . -name *.js -size +1M -ls
+sudo find . -name '*WenttoPakista*.mp4' -o -name '*IAMtheeart*.mp4'
+find . -name "*M*" ! -name "Mov*" -maxdepth 1
+find . -path './baas/treeQL' -prune -o -path './baas/*' # don't find inside ./baas/treeQL but ./baas/*
